1b752c923ed13fd3bedc83a6252b91e17a87c3f3
CH Praveen <40192928+2chpraveen123@users.noreply.github.com>
2026-01-09 16:15:11 +0530

    Links seperated

--- script.js
+++ script.js
@@ -194,14 +194,8 @@ function randomBlink() {
    TEAR LINKS (MEMORY RANDOM)
 ----------------------------- */
 const tearLinks = [
-  "https://opensea.io/item/polygon/0x164ba817278d4308be80b6afe4f7ab55f5aea88c/27",
-  "https://opensea.io/item/polygon/0x164ba817278d4308be80b6afe4f7ab55f5aea88c/53",
-  "https://opensea.io/item/polygon/0x164ba817278d4308be80b6afe4f7ab55f5aea88c/173",
-  "https://opensea.io/item/polygon/0x164ba817278d4308be80b6afe4f7ab55f5aea88c/28",
-  "https://medium.com/@qmbgjhq/title-when-force-meets-flow-d8baed6d5020",
-  "https://medium.com/@qmbgjhq/the-invertible-mind-3225a7ac2b69",
-  "https://medium.com/@qmbgjhq/truth-of-scale-45c57c90ed7b",
-  "https://medium.com/@qmbgjhq/false-prophecy-to-truth-cab69298b39a"
+  ...window.OPEN_SEA_LINKS,
+  ...window.MEDIUM_LINKS
 ];
 
 // working memory pool
