ef99dd1250d94278f9dc89df3d3cf1ff02560a5f
CH Praveen <40192928+2chpraveen123@users.noreply.github.com>
2026-01-11 22:49:57 +0530

    Added 4

--- links/medium-links.js
+++ links/medium-links.js
@@ -3,6 +3,7 @@
 window.MEDIUM_LINKS = [
   "https://medium.com/@qmbgjhq/title-when-force-meets-flow-d8baed6d5020",
   "https://medium.com/@qmbgjhq/point-of-truth-the-love-4e2116782d5e",
+  "https://medium.com/@qmbgjhq/the-light-within-the-broken-heart-b890fc22f5ba",
 
   //Untitled collection and intial links
   "https://medium.com/@qmbgjhq/the-invertible-mind-3225a7ac2b69", //Untitled collection
