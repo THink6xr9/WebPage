d9b47e05e4e7abf758b86376e9851a85d0a7d0bf
CH Praveen <2praveench123@gmail.com>
2026-01-21 19:22:08 +0530

    missing ,

--- src/data/opensea-links.js
+++ src/data/opensea-links.js
@@ -9,7 +9,7 @@ export const OPEN_SEA_LINKS = [
   "https://opensea.io/item/polygon/0x164ba817278d4308be80b6afe4f7ab55f5aea88c/9",
   "https://opensea.io/item/polygon/0x164ba817278d4308be80b6afe4f7ab55f5aea88c/10",
   "https://opensea.io/item/polygon/0x164ba817278d4308be80b6afe4f7ab55f5aea88c/11",
-  "https://opensea.io/item/polygon/0x164ba817278d4308be80b6afe4f7ab55f5aea88c/12"
+  "https://opensea.io/item/polygon/0x164ba817278d4308be80b6afe4f7ab55f5aea88c/12",
 
   //need to be added in images folder and intial links
   "https://opensea.io/item/polygon/0x164ba817278d4308be80b6afe4f7ab55f5aea88c/27",//need to be added in images folder
