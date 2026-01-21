25f4a3f8eeb87a6c92cad338d749b2b22398e441
CH Praveen <2praveench123@gmail.com>
2026-01-21 18:33:46 +0530

    added 12

--- src/data/medium-links.js
+++ src/data/medium-links.js
@@ -9,6 +9,7 @@ export const MEDIUM_LINKS = [
   "https://medium.com/@qmbgjhq/the-safer-shadows-within-ef7969c50cc3",
   "https://medium.com/@qmbgjhq/the-gift-of-gratitude-49a9d0622c3b",
   "https://medium.com/@qmbgjhq/from-fire-to-light-721a600a2d7e",
+  "https://medium.com/@qmbgjhq/the-gift-of-love-89880117186d",
 
   //Untitled collection and intial links
   "https://medium.com/@qmbgjhq/the-invertible-mind-3225a7ac2b69", //Untitled collection
