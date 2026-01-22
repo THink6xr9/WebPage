02d69182fe4314b56b4167c9657a19c5b72bb7e3
CH Praveen <2praveench123@gmail.com>
2026-01-22 13:34:55 +0530

    minor removed ,

--- src/data/playlist.js
+++ src/data/playlist.js
@@ -5,7 +5,7 @@ export const playlist = [
     {
         title: "Open Hands, Open Heart",
         src: "music/Open-Hands-Open-Heart.mp3"
-    },
+    }
     // {
     //     title: "In the Quiet of Forever",
     //     src: "music/In-the-Quiet-of-Forever.mp3"
