1f17250f661aa406e5de4378012d86c2a7b04471
CH Praveen <2praveench123@gmail.com>
2026-02-01 19:03:00 +0530

    added 19-20

--- src/data/medium-links.js
+++ src/data/medium-links.js
@@ -16,6 +16,8 @@ export const MEDIUM_LINKS = [
   "https://medium.com/@qmbgjhq/love-holding-hunger-ed3fc0b0d07d",
   "https://medium.com/@qmbgjhq/roots-of-wholeness-9bedd68b32b8",
   "https://medium.com/@qmbgjhq/the-lie-in-loneliness-5736d48252cb",
+  "https://medium.com/@qmbgjhq/the-union-of-truths-8f5e6697e01b",
+  "https://medium.com/@qmbgjhq/the-tear-of-hope-f745fce17b91",
 
   //Untitled collection and intial links
   "https://medium.com/@qmbgjhq/the-invertible-mind-3225a7ac2b69", //Untitled collection
