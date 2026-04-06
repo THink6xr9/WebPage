d2ad3391709c0884aa5056f8e57c62ac7060f110
CH Praveen <2praveench123@gmail.com>
2026-04-06 19:27:54 +0530

    Added Embedded Youtube Player and Words

---
+++ src/data/face-words.js
@@ -0,0 +1,6 @@
+export const FACE_WORDS = [
+    "Truth",
+    "Lie",
+    "Illusion",
+    "Clarity"
+];
--- src/data/youtube-links.js
+++ src/data/youtube-links.js
@@ -6,5 +6,24 @@ export const YOUTUBE_LINKS = [
     "https://youtu.be/R8Xnb5o7bww", //15
     "https://youtu.be/4dKhN8mEiZI", //16
     "https://youtu.be/mDbXJAtvkno", //17
-    "https://www.youtube.com/watch?v=d1pdDfKUTG0" //18
+    "https://www.youtube.com/watch?v=d1pdDfKUTG0", //18
+    "https://youtu.be/-TutVYNbTOY", //19
+    "https://youtu.be/T25cVHiyQ_Q", //20
+    "https://www.youtube.com/watch?v=0PZLi3f-O38", //21
+    "https://www.youtube.com/watch?v=ri99hOI5OyY", //22
+    "https://youtu.be/44cXrAGGfqw", //23
+    "https://youtu.be/UNIPf2_pvoA", //24
+    "https://youtu.be/c7T74F9N-74", //25
+    "https://youtu.be/xDB8uxhU_o0", //26
+    "https://youtu.be/UM-ZG8bQ3Ds", //27
+    "https://youtu.be/0plIwEpXclQ", //28
+    "https://youtu.be/0plIwEpXclQ", //29
+    "https://youtu.be/T843JsKKJsM", //30
+    "https://www.youtube.com/watch?v=N_SLJthaJjE", //31
+    "https://youtu.be/XgLiw8kjqQE", //32
+    "https://youtu.be/uNLZ-VCVYII", //33
+    "https://youtu.be/trlgnHuVB9A", //34
+    "https://youtu.be/szEiqiiyT_E", //35
+    "https://youtu.be/WigwTQyzhB4", //36
+    "https://youtu.be/mpWGau6WJ60" //37
 ];
